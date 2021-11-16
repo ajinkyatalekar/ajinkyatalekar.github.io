f2ed21a57101d02763f4590577c60896ac177f29
Ajinkya Talekar <91043799+ajinkyatalekar@users.noreply.github.com>
2021-11-16 19:37:03 +0530

    Removed SVG background

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
 function toggleDisplay(dropdownID) {
-    var x = document.getElementById(dropdownID);
-    if (x.style.display === "block") {
-      x.style.display = "none";
-    } else {
-      x.style.display = "block";
-    }
+  var x = document.getElementById(dropdownID);
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
   }
+}
 
 var y = 0;
 function toggleBackground() {
   var x = document.body;
   if (y == 0) {
-    x.style.backgroundImage = "url('images/epsilonnaut.svg')";
+    x.style.backgroundImage = "url('images/none.svg')";
     y = 1;
   } else {
     x.style.backgroundImage = "url('images/2.svg')";
@@ -20,8 +20,8 @@ function toggleBackground() {
 }
 
 
-  // window.onload = function() {
-  //   var images = ['images/1.svg', 'images/2.svg', 'images/3.svg'];
-  //   var image = images[Math.floor(Math.random() * images.length)]
-    document.getElementsByTagName('body')[0].style.backgroundImage = "url('" + image + "')";
+// window.onload = function() {
+//   var images = ['images/1.svg', 'images/2.svg', 'images/3.svg'];
+//   var image = images[Math.floor(Math.random() * images.length)]
+document.getElementsByTagName('body')[0].style.backgroundImage = "url('" + image + "')";
   // }
